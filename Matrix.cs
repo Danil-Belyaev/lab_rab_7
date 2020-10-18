155ff7122c5fad90e8f6ede771c0f2ace3277810
Danil-Belyaev <70798431+Danil-Belyaev@users.noreply.github.com>
2020-10-18 16:09:34 +0300

    Add files via upload

---
+++ Matrix.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Перегрузки
+{
+    class Matrix
+    {
+        int[,] mas;
+        public Matrix(int N, int M)
+        {
+            this.M = M;
+            this.N = N;
+            mas = new int[N, M];
+
+            Random rand = new Random();
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    mas[i, j] = Convert.ToInt32(rand.Next(100));
+                }
+            }
+        }
+
+        public Matrix() : this(1, 1)
+        {
+
+        }
+
+        public Matrix(Matrix a)
+        {
+            mas = new int[a.M, a.N];
+            // скопировать все из a в mas
+        }
+
+        public int N;
+        public int M;
+
+
+        public void Print(string p)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    Console.Write(mas[i, j] + p);
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        public void Print()
+        {
+            Print("\t");
+        }
+
+        public int Max()
+        {
+            int maximal = mas[0, 0];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    if (mas[i, j] > maximal)
+                    { maximal = mas[i, j]; }
+                }
+            }
+            return maximal;
+        }
+
+        public int Min()
+        {
+            int minimal = mas[0, 0];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    if (mas[i, j] < minimal)
+                    { minimal = mas[i, j]; }
+                }
+            }
+            return minimal;
+        }
+
+        public int Min(int i)
+        {
+            if (i < N)
+            {
+                int minimal = mas[0, 0];
+                for (int j = 0; j < M; j++)
+                {
+                    if (mas[i, j] < minimal)
+                    { minimal = mas[i, j]; }
+                }
+
+                return minimal;
+            }
+            else return 0;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+namespace Перегрузки
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if DEBUG
+            int N;
+            int M;
+#endif
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"output.txt");
+            var new_in = new StreamReader(@"input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+
+            int N = Convert.ToInt32(Console.ReadLine());
+            int M = Convert.ToInt32(Console.ReadLine());
+#endif
+
+#if DEBUG
+            Console.Write("Введите высоту матрицы  0<=N<=25 > ");
+            N = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите ширину матрицы  0<=M<=25 > ");
+            M = Convert.ToInt32(Console.ReadLine());
+#endif
+            Console.WriteLine();
+            Matrix a = new Matrix(N, M);
+
+            a.Print();
+
+
+            Console.WriteLine("Минимальный элемент матрицы : ");
+            Console.WriteLine(a.Min());
+
+
+            Console.WriteLine("Максимальный элемент матрицы : ");
+            Console.WriteLine(a.Max());
+#if DEBUG
+            Console.ReadKey();
+#endif
+
+# if !DEBUG
+            Console.SetOut(save_out); new_out.Close();
+            Console.SetIn(save_in); new_in.Close();
+# endif
+        }
+    }
+}
